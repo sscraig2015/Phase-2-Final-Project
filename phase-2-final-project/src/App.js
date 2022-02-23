ec0430b8677c58febcd4e97ccdd75e738f7efd2b
Sean <craig.sean92@gmail.com>
2022-02-22 18:33:41 -0700

    import data (test)

--- phase-2-final-project/src/App.js
+++ phase-2-final-project/src/App.js
@@ -1,12 +1,24 @@
-
 import './App.css';
-import movies from "./db.json"
+import React, {useEffect, useState} from "react"
+
 
 function App() {
+  const [movies, setMovies] = useState(null)
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+        .then((resp) => resp.json())
+        .then((data) => setMovies(data))
+        
+  }, [])
+  
   return (
-    <div className="App">
-      {console.log(movies[2])}
+    <div>
+      {movies.map((movie) => 
+        console.log(movie)
+      )}
     </div>
+    
   );
 }
 
